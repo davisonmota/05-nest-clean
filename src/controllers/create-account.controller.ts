ce2a6c1869c4f64c34ff589a139dec7e9595a415
Davison Mota <davimotagnr@gmail.com>
2024-07-31 13:24:38 -0300

    Criando Pipe de validação do Zod

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -4,9 +4,20 @@ import {
   Controller,
   HttpCode,
   Post,
+  UsePipes,
 } from '@nestjs/common';
 import { hash } from 'bcryptjs';
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { z } from 'zod';
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+});
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>;
 
 @Controller('/accounts')
 export class CreateAccountController {
@@ -14,17 +25,10 @@ export class CreateAccountController {
 
   @Post()
   @HttpCode(201)
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
   async handle(
     @Body()
-    {
-      name,
-      email,
-      password,
-    }: {
-      name: string;
-      email: string;
-      password: string;
-    },
+    { name, email, password }: CreateAccountBodySchema,
   ) {
     const userWithSameEmail = await this.prisma.user.findUnique({
       where: {
---
+++ src/pipes/zod-validation-pipe.ts
@@ -0,0 +1,28 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common';
+import { ZodError, ZodSchema } from 'zod';
+import { fromZodError } from 'zod-validation-error';
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema) {}
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  transform(value: unknown, metadata: ArgumentMetadata) {
+    try {
+      const parsedValue = this.schema.parse(value);
+      return parsedValue;
+    } catch (error) {
+      if (error instanceof ZodError) {
+        throw new BadRequestException({
+          errors: fromZodError(error),
+          message: 'Validation failed',
+          statusCode: 400,
+        });
+      }
+      throw new BadRequestException('Validation failed');
+    }
+  }
+}
