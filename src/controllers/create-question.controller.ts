7fd29260fcc02ff1d0afbf46473a06bad47b570c
Davison Mota <davimotagnr@gmail.com>
2024-08-01 09:38:33 -0300

    Controller de criação de pergunta

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,17 +1,18 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { CurrentUser } from 'src/auth/current-user-decorator';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { UserPayload } from 'src/auth/jwt.strategy';
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { z } from 'zod';
 
-// const createQuestionBodySchema = z.object({
-//   title: z.string(),
-//   content: z.string(),
-//   authorId: z.string().uuid(),
-// });
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+});
 
-// type createQuestionBodySchema = z.infer<typeof createQuestionBodySchema>;
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>;
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
@@ -22,9 +23,33 @@ export class CreateQuestionController {
   ) {}
 
   @Post()
-  // @UsePipes(new ZodValidationPipe(createQuestionBodySchema))
-  async handle(@CurrentUser() user: UserPayload) {
-    console.log(user.sub);
-    return 'ok';
-  } // @Body() { authorId, title, content }: createQuestionBodySchema,
+  async handle(
+    @CurrentUser() user: UserPayload,
+    @Body(new ZodValidationPipe(createQuestionBodySchema))
+    { title, content }: CreateQuestionBodySchema,
+  ) {
+    const userId = user.sub;
+    const slug = this.converteToSlug(title);
+
+    await this.prismaService.question.create({
+      data: {
+        title,
+        content,
+        slug,
+        authorId: userId,
+      },
+    });
+  }
+
+  private converteToSlug(title: string) {
+    return title
+      .normalize('NFKD')
+      .toLocaleLowerCase()
+      .trim()
+      .replace(/\s+/g, '-')
+      .replace(/[^\w]+/g, '-')
+      .replace(/_/g, '-')
+      .replace(/--/g, '-')
+      .replace(/-$/g, '');
+  }
 }
