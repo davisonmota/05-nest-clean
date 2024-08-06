f61f40505a7760bffc4b3c01ee3ffc3ffe1c61fa
Davison Mota <davimotagnr@gmail.com>
2024-08-06 12:58:28 -0300

    Rotas privadas por padrão

--- src/infra/auth/jwt-auth.guard.ts
+++ src/infra/auth/jwt-auth.guard.ts
@@ -1,3 +1,58 @@
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { Env } from '../env';
+import { IS_PUBLIC_KEY } from './public';
 
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector,
+    private readonly configService: ConfigService<Env, true>,
+  ) {
+    super();
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      // 💡 See this condition
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        algorithms: ['RS256'],
+        publicKey: Buffer.from(
+          this.configService.get<string>('JWT_PUBLIC_KEY'),
+          'base64',
+        ),
+      });
+      request['user'] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
--- src/infra/http/controllers/fetch-recent-question.controller.ts
+++ src/infra/http/controllers/fetch-recent-question.controller.ts
@@ -1,12 +1,5 @@
 import { FetchRecentQuestionUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions';
-import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard';
-import {
-  BadRequestException,
-  Controller,
-  Get,
-  Query,
-  UseGuards,
-} from '@nestjs/common';
+import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
 import { z } from 'zod';
 import { ZodValidationPipe } from '../pipes/zod-validation-pipe';
 import { QuestionPresenter } from '../presenters/question-presenter';
@@ -17,7 +10,6 @@ const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema);
 type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>;
 
 @Controller('/questions')
-@UseGuards(JwtAuthGuard)
 export class FetchRecentQuestionsController {
   constructor(
     private readonly fetchRecentQuestionUseCase: FetchRecentQuestionUseCase,
