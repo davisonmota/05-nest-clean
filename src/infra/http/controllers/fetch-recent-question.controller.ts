8d1fe4a5173ec3c833c6986d8402dc21518ff17b
Davison Mota <davimotagnr@gmail.com>
2024-08-06 12:25:24 -0300

    Tratando erros nos controllers

--- src/infra/http/controllers/fetch-recent-question.controller.ts
+++ src/infra/http/controllers/fetch-recent-question.controller.ts
@@ -1,6 +1,12 @@
 import { FetchRecentQuestionUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions';
 import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard';
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { z } from 'zod';
 import { ZodValidationPipe } from '../pipes/zod-validation-pipe';
 import { QuestionPresenter } from '../presenters/question-presenter';
@@ -22,8 +28,9 @@ export class FetchRecentQuestionsController {
     const result = await this.fetchRecentQuestionUseCase.execute({ page });
 
     if (result.isLeft()) {
-      throw new Error();
+      throw new BadRequestException();
     }
+
     const { questions } = result.value;
 
     return { questions: questions.map(QuestionPresenter.toHTTP) };
